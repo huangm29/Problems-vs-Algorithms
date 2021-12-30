5fdefb8c848abdfad3b256b80151823bcc2d3b0c
Meng Huang <huangm1065@gmail.com>
2021-12-29 23:53:23 -0500

    Initial code for problem 1-5

---
+++ problem_1.py
@@ -0,0 +1,34 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    #special case
+    if (number == 0 or number == 1) :
+        return number
+
+    start = 0
+    stop = number
+    mid = (start + stop) // 2
+
+    while(start != stop):
+        if (mid * mid == number):
+            return mid
+        elif (mid * mid < number):
+            start = mid + 1
+        else:
+            stop = mid - 1
+        mid = (start + stop) // 2
+    return mid
+            
+
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
---
+++ problem_4.py
@@ -0,0 +1,35 @@
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    left_index = 0
+    right_index = len(input_list)-1
+    index = 0 
+    while index <= right_index:
+        if input_list[index] == 0:
+            input_list[index] = input_list[left_index]
+            input_list[left_index] = 0
+            index += 1
+            left_index += 1
+        elif input_list[index] == 2:
+            input_list[index] = input_list[right_index]
+            input_list[right_index] = 2
+            right_index -= 1
+        else:
+            index += 1
+    return input_list
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
